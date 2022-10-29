fbf646f1dbd5212d6127ed313370978e75b1e897
mehmet-akif-tanisik <matnsk@hotmail.com>
2022-10-29 18:49:31 +0300

    commit for taxiBillCalculatorProgram

---
+++ Java-101/src/taxiBillCalculatorProgram/Main.java
@@ -0,0 +1,30 @@
+package taxiBillCalculatorProgram;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int distance;
+        double perKm, totalBill, startingPrice;
+
+        perKm = 2.20;
+        startingPrice = 10;
+
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Please enter distance as km: ");
+        distance = keyboard.nextInt();
+
+        totalBill = (distance * perKm);
+        totalBill += startingPrice;
+
+        totalBill = (totalBill<20) ? 20 : totalBill;
+        System.out.println("Total bill: " + totalBill);
+
+
+
+
+    }
+
+}
