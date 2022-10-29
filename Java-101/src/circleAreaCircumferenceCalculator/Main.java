3728677227f9288333d160896a2d32a1af1c9ad4
mehmet-akif-tanisik <matnsk@hotmail.com>
2022-10-29 19:01:32 +0300

    commit for areaSegmentCalculator

---
+++ Java-101/src/circleAreaCircumferenceCalculator/Main.java
@@ -0,0 +1,27 @@
+package circleAreaCircumferenceCalculator;
+
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        int radius;
+        double area, circumference, pi;
+        pi = 3.14;
+
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Please enter radius of the circle: ");
+        radius = keyboard.nextInt();
+
+        area = pi * radius * radius;
+        circumference = 2 * pi * radius;
+
+        System.out.printf("Area of circle: %.2f%n" +
+                           "Circumference of circle: %.2f",area,circumference );
+
+    }
+}
---
+++ Java-101/src/circleAreaCircumferenceCalculator/Segment.java
@@ -0,0 +1,30 @@
+package circleAreaCircumferenceCalculator;
+
+import java.util.Scanner;
+
+public class Segment {
+
+
+    public static void main(String[] args) {
+
+        int radius, centralAngle;
+        double area, pi;
+
+        pi = 3.14;
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Please enter radius of the circle: ");
+        radius = keyboard.nextInt();
+
+        System.out.print("Please enter central angle of the segment: ");
+        centralAngle = keyboard.nextInt();
+
+        area = (pi * (radius*radius) * centralAngle)/360.0;
+
+        System.out.println("Area: " + area);
+
+
+
+    }
+}
