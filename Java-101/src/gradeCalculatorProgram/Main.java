19c4905fdf7c5d86ffe0c351a0ab77f8dd860308
mehmet-akif-tanisik <matnsk@hotmail.com>
2022-10-29 17:57:26 +0300

    commit for gradeCalculatorProgram

---
+++ Java-101/src/gradeCalculatorProgram/Main.java
@@ -0,0 +1,42 @@
+package gradeCalculatorProgram;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int mathematics, physics, chemistry, turkish, history, music;
+        double avarageGrade;
+
+        Scanner keyboard = new Scanner(System.in);
+
+
+        System.out.print("Enter grade of mathematics lecture: ");
+        mathematics = keyboard.nextInt();
+
+        System.out.print("Enter grade of physics lecture: ");
+        physics = keyboard.nextInt();
+
+        System.out.print("Enter grade of chemistry lecture: ");
+        chemistry = keyboard.nextInt();
+
+        System.out.print("Enter grade of turkish lecture: ");
+        turkish = keyboard.nextInt();
+
+        System.out.print("Enter grade of history lecture: ");
+        history = keyboard.nextInt();
+
+        System.out.print("Enter grade of music lecture: ");
+        music = keyboard.nextInt();
+
+        avarageGrade = (mathematics + physics + chemistry + turkish + history + music) / 6.0;
+
+        System.out.printf("Avarage grade of 6 courses : %.2f%n",avarageGrade);
+        System.out.println( avarageGrade>=60 ? "Passed" : "Failed");
+
+
+
+
+    }
+}
