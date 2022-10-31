435141ed217f0edc8070c6e23f601d9bc5e6a698
mehmet-akif-tanisik <matnsk@hotmail.com>
2022-10-31 20:22:01 +0300

    Added comment lines for BmiCalculator

--- Java-101/src/homeworks/BmiCalculator.java
+++ Java-101/src/homeworks/BmiCalculator.java
@@ -7,19 +7,25 @@ public class BmiCalculator {
 
     public static void main(String[] args) {
 
-        double weight, height; //weight in kg, height in meters
+        //weight in kg, height in meters
+        double weight, height;
+        // basal metabolic rate
         double bmi;
 
+        //keyboard variable is a Scanner object that will read input from keyboard.
         Scanner keyboard = new Scanner(System.in);
 
+        //Taking inputs from user with "keyboard" Scanner object.
         System.out.print("Please enter your height(as meter e.q: 1.72): ");
         height = keyboard.nextDouble();
 
         System.out.print("Please enter your weight(as kilogram e.q: 76): ");
         weight = keyboard.nextDouble();
 
+        //Basal Metabolic Rate = Weight(kg) / (Height(meters) * Height(meters))
         bmi = weight / (height*height);
 
+        //Screen output to show basal metabolic rate of user.
         System.out.printf("Your BMI is: %.2f", bmi);
 
 
