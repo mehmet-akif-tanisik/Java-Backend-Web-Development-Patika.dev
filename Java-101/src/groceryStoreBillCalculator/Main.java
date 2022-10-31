012340230e61baaf24db5b89a79221fe706f8b77
mehmet-akif-tanisik <matnsk@hotmail.com>
2022-10-31 17:03:04 +0300

    commit for groceryStoreBillCalculator

---
+++ Java-101/src/groceryStoreBillCalculator/Main.java
@@ -0,0 +1,34 @@
+package groceryStoreBillCalculator;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner kb = new Scanner(System.in);
+
+        double apple, pear, tomato, eggplant, banana, billAmount;
+
+        System.out.print("Apple kg: ");
+        apple = kb.nextDouble();
+
+        System.out.print("Pear kg: ");
+        pear = kb.nextDouble();
+
+        System.out.print("Tomato kg: ");
+        tomato = kb.nextDouble();
+
+        System.out.print("Eggplant kg: ");
+        eggplant = kb.nextDouble();
+
+        System.out.print("Banana kg: ");
+        banana = kb.nextDouble();
+
+        billAmount = (apple * 3.67) + (pear * 2.14) + (tomato * 1.11) + (eggplant * 5) + (banana * 0.95);
+
+        System.out.print("Total bill amount: " + billAmount);
+
+
+    }
+}
