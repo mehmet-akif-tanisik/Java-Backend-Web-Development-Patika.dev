69e0cfc3cfe9391afb6ef3d5a30ba9f8db033311
mehmet-akif-tanisik <matnsk@hotmail.com>
2022-10-31 20:37:11 +0300

    Added comment lines for GroceryStoreBillCalculator

--- Java-101/src/homeworks/GroceryStoreBillCalculator.java
+++ Java-101/src/homeworks/GroceryStoreBillCalculator.java
@@ -6,6 +6,9 @@ public class GroceryStoreBillCalculator {
 
     public static void main(String[] args) {
 
+        //Declaration and initializing cost of the products
+        double appleCost=3.67, pearCost=2.14, tomatoCost=1.11, eggplantCost=5, bananaCost=0.95;
+
         // Declaration of products and total bill amount
         double apple, pear, tomato, eggplant, banana, billAmount;
 
@@ -29,7 +32,7 @@ public class GroceryStoreBillCalculator {
         banana = keyboard.nextDouble();
 
         //Calculation of total bill amount and initialize it to billAmount variable.
-        billAmount = (apple * 3.67) + (pear * 2.14) + (tomato * 1.11) + (eggplant * 5) + (banana * 0.95);
+        billAmount = (apple * appleCost) + (pear * pearCost) + (tomato * tomatoCost) + (eggplant * eggplantCost) + (banana * bananaCost);
 
         //Screen output to show total amount of bill.
         System.out.print("Total bill amount: " + billAmount);
