48f1e2fb1381db03575dbff6f936cee9d8043b51
mehmet-akif-tanisik <matnsk@hotmail.com>
2022-10-31 20:32:44 +0300

    Added comment lines for GroceryStoreBillCalculator

--- Java-101/src/homeworks/GroceryStoreBillCalculator.java
+++ Java-101/src/homeworks/GroceryStoreBillCalculator.java
@@ -6,27 +6,32 @@ public class GroceryStoreBillCalculator {
 
     public static void main(String[] args) {
 
-        Scanner kb = new Scanner(System.in);
-
+        // Declaration of products and total bill amount
         double apple, pear, tomato, eggplant, banana, billAmount;
 
+        //Initializing "keyboard" object from Scanner class that will read input from keyboard.
+        Scanner keyboard = new Scanner(System.in);
+
+        //Taking inputs apple, pear, tomato, eggplant and banana kilograms as double from user using "keyboard" object.
         System.out.print("Apple kg: ");
-        apple = kb.nextDouble();
+        apple = keyboard.nextDouble();
 
         System.out.print("Pear kg: ");
-        pear = kb.nextDouble();
+        pear = keyboard.nextDouble();
 
         System.out.print("Tomato kg: ");
-        tomato = kb.nextDouble();
+        tomato = keyboard.nextDouble();
 
         System.out.print("Eggplant kg: ");
-        eggplant = kb.nextDouble();
+        eggplant = keyboard.nextDouble();
 
         System.out.print("Banana kg: ");
-        banana = kb.nextDouble();
+        banana = keyboard.nextDouble();
 
+        //Calculation of total bill amount and initialize it to billAmount variable.
         billAmount = (apple * 3.67) + (pear * 2.14) + (tomato * 1.11) + (eggplant * 5) + (banana * 0.95);
 
+        //Screen output to show total amount of bill.
         System.out.print("Total bill amount: " + billAmount);
 
 
