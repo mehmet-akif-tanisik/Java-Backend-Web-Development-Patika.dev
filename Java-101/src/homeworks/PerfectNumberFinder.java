f2055af66c9cdfa6a30904964b05de4125b9411f
mehmet-akif-tanisik <matnsk@hotmail.com>
2022-11-02 15:46:10 +0300

    PerfectNumberFinder.java added to homeworks

--- Java-101/src/homeworks/PerfectNumberFinder.java
+++ Java-101/src/homeworks/PerfectNumberFinder.java
@@ -17,40 +17,6 @@ public class PerfectNumberFinder {
                 if (val % i == 0)
                     sum += i;
             }
-
             System.out.println(sum == val ? "Perfect Number!" : "Not a Perfect Number!");
-
-
-
-            System.out.print("Please enter a number: ");
-            int val2 = keyboard.nextInt();
-
-            int sum2 = 0;
-
-            for (int i = 1; i <= val2 / 2; ++i) {
-                if (val2 % i == 0)
-                    sum2 += i;
-            }
-
-            System.out.println(sum2 == val2 ? "Perfect Number!" : "Not a Perfect Number!");
-
-
-
-            System.out.print("Please enter a number: ");
-            int val3 = keyboard.nextInt();
-
-            int sum3 = 0;
-
-            for (int i = 1; i <= val3 / 2; ++i) {
-                if (val3 % i == 0)
-                    sum3 += i;
-            }
-
-            System.out.println(sum3 == val3 ? "Perfect Number!" : "Not a Perfect Number!");
-
-
-
-
-
         }
 }
